75e284ca6d02a8fa63abeb076ae5371d49d4b4b6
Gregory-JP <63425849+Gregory-JP@users.noreply.github.com>
2022-11-02 12:24:19 -0300

    Sintax error fixed

--- main_program.py
+++ main_program.py
@@ -1,38 +1,15 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[4]:
-
-
 from pytube import YouTube
-
-
-# In[8]:
-
 
 def music_download(link):
     music = YouTube(link)
     stream = music.streams.get_audio_only()
     stream.download()
-
-
-# In[9]:
-
-
-music_download('link aqui')
-
-
-# In[10]:
-
-
+    
 def video_download(link):
     video = YouTube(link)
     stream = video.streams.get_highest_resolution()
     stream.download()
 
-
-# In[ ]:
-
+music_download('link aqui')
 
 video_download('link aqui')
-
