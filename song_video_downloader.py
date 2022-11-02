00697f84a968a4a24c55d5cd5f829017df421fb4
Gregory-JP <63425849+Gregory-JP@users.noreply.github.com>
2022-11-02 12:27:51 -0300

    Small fixes

--- song_video_downloader.py
+++ song_video_downloader.py
@@ -1,10 +1,12 @@
 from pytube import YouTube
 
+# download only the sound
 def music_download(link):
     music = YouTube(link)
     stream = music.streams.get_audio_only()
     stream.download()
-    
+
+# download the video with the audio
 def video_download(link):
     video = YouTube(link)
     stream = video.streams.get_highest_resolution()
